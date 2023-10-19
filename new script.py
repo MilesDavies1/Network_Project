3797e9db37443783c0f4595d37b792c6698e5182
MilesDavies1 <104517485+MilesDavies1@users.noreply.github.com>
2023-10-19 12:42:11 -0400

    Update new script.py

--- new script.py
+++ new script.py
@@ -30,3 +30,5 @@ if __name__ == "__main__":
         traceroute_df = pd.DataFrame(traceroute_data) # creates a pandas dataframe and adds the dictionary info
         traceroute_df.to_excel('traceroute_results.xlsx', index=False)  # pushes the collected dataframe info to an excel document named traceroute_xlsx
         print(f"Traceroute to {target_ip}: {result}")
+
+#Todo: use google dns, remove duplicate results, use multithreading(200 threads), split ranges per group member, write seperate program to merge all results
